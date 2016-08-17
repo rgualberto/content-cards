47c6dfff9e628d8388835aabe6c3be320636c3be
Rodrigo Gualberto <itzdrigz@gmail.com>
2016-08-16 22:33:17 -0400

    Add basic styles

--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -3,10 +3,12 @@ import React, {PropTypes} from 'react';
 class TodoCard extends React.Component {
   render() {
     const {cardData} = this.props;
+    const imageClass = "card__image card__image--" + cardData.image;
 
     return (
       <div className="card">
         <div className="card__container">
+          <div className={imageClass}></div>
           <h2 className="card__title">{cardData.title}</h2>
           <div className="card__content">
             <ul>
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -2,17 +2,20 @@ export default {
   todoCards: [
     {
       title: 'First Card',
-      todos: ['Go to work', 'Come back Home', 'Go to sleep']
+      todos: ['Go to work', 'Come back Home', 'Go to sleep'],
+      image: 'stock-1'
     },
 
     {
       title: 'Second Guy',
-      todos: ['Somewhere', 'Over the rainbow']
+      todos: ['Somewhere', 'Over the rainbow'],
+      image: 'stock-2'
     },
 
     {
       title: 'Third Dude',
-      todos: ['I like', 'to eat', 'food with', 'several hot', 'peppers and', 'beans']
+      todos: ['I like', 'to eat', 'food with', 'several hot', 'peppers and', 'beans'],
+      image: 'stock-3'
     }
   ]
 };
